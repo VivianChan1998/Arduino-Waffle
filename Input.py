f46b4154cce7da830b182fcb0aabc9737585b328
VivianChan1998 <vhchan@berkeley.edu>
2024-08-04 10:41:18 -0700

    deleted unused files

--- Input.py
+++
@@ -1,8 +0,0 @@
-import Component
-
-class InputDevice(Component):
-    def __init__(self, name: str, isDigital: bool=True, isAnalog: bool=False):
-        self.isDigital = isDigital
-        self.isAnalog = isAnalog
-
-
--- Library.py
+++
@@ -1,3 +0,0 @@
-class Library:
-    def __init__(self, name: str):
-        self.name = name
